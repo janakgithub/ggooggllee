d8f23d09d5ec62574385228d666b3a4619705534
janakgithub <janak4u@gmail.com>
2023-09-04 23:25:18 +0930

    light and dark mode working

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ export const App = () => {
     const [darkTheme, setDarkTheme] = useState(false);
     return (
         <div className={darkTheme? 'dark': ''}>
-            <Navbar />
-            <Routes />
-            <Footer />
+            <div className='bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen'>
+                <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
+                <Routes />
+                <Footer />
+            </div>    
         </div>
     );
 }
